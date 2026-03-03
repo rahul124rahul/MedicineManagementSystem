a505f3286d0e38f2746c5c33234084744e66e1b2
Rahul Ganesh Dadge <rahuldadge2003@gmail.com>
2026-03-03 22:42:33 +0530

    Initialized database and registered models

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask
 from config import Config
 from models import db
+from models.patient import Patient
+from models.medicine import Medicine
+from models.dose import Dose
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db.init_app(app)
 
+# Create tables automatically
 with app.app_context():
     db.create_all()
+
+@app.route("/")
+def home():
+    return "Medicine Management System Backend Running"
+
+if __name__ == "__main__":
+    app.run(debug=True)
